4254aaf3e591e23dce5559daf6ef2b580a875f3d
nicolaspz07 <nicolaspz2006@gmail.com>
2025-08-12 22:57:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // --- CONFIGURAÇÃO IMPORTANTE ---
 // Coloque a data de início do namoro aqui no formato: ANO-MÊS-DIA
-const dataInicioNamoro = new Date('2023-10-25T20:00:00'); // Exemplo: 25 de Outubro de 2023 às 20h
+const dataInicioNamoro = new Date('2024-09-07 23:00:00'); // Exemplo: 25 de Outubro de 2023 às 20h
 
 // Coloque o nome das suas fotos aqui
 const listaDeFotos = [
     'imagens/foto1.jpg',
     'imagens/foto2.jpg',
-    'imagens/foto3.png',
+    'imagens/foto3.jpg',
+    'imagens/foto4.jpg',
+    'imagens/foto5.jpg',
     // Adicione quantas fotos quiser!
 ];
 
